1e56b8a1304d5ea2a377809eedcd82cd588dc664
Anthony <tryout3699@gmail.com>
2024-09-08 13:17:54 -0300

    feat Adding to entrypoint new dependencies installed for maitenence data of logged user

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import * as session from 'express-session';
+import * as passport from 'passport';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   await app.listen(3000);
 }
 bootstrap();
