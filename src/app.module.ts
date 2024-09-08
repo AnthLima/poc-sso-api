6591c84fa4126f85b228f195f5e668c29c242e40
Anthony <tryout3699@gmail.com>
2024-09-08 18:33:19 -0300

    feat: Adding new modules to app module, like config, passport and jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule, 
+    ConfigModule.forRoot(),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
