20866f432a28469ed6b4f31a413279ca4c26349b
Anthony <tryout3699@gmail.com>
2024-09-09 00:45:13 -0300

    feat: Adding new uses for application to configure no cors origin and application cookie based

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   const configService = app.get(ConfigService);
-  
+
+  app.use(cors({
+    origin: configService.get<string>('ORGIGIN_FOR_NO_CORS'),
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+
+  app.use(cookieParser());
+
   app.use(
     session({
       secret: configService.get<string>('SESSION_SECRET'),
