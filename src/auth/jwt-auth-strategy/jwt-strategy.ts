4db8d51cd2c9ada23228a8e3c8be56d84b343615
Anthony <tryout3699@gmail.com>
2024-09-09 00:45:47 -0300

    feat: Create new jwt auth guard with name of cookie like jwt

--- src/auth/jwt-auth-strategy/jwt-strategy.ts
+++ src/auth/jwt-auth-strategy/jwt-strategy.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
-    });
-  }
+    constructor(configService: ConfigService ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                JwtStrategy.extractJWT,
+                ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ]),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET'),
+        });
+    }
 
-  async validate(payload: any) {
-    return { userId: payload.sub, email: payload.email };
-  }
+    private static extractJWT(req: Request | any): string | null {
+        console.log("REQ",req.cookies['jwt'])
+
+        return req.cookies['jwt'];
+    }
+
+    async validate(payload: any) {
+        return { userId: payload.sub, username: payload.username };
+    }
 }
